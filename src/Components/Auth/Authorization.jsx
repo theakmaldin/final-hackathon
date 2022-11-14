318d7e57d447a8f7ce7694c5e4451d471f174e69
Akmaldin <akmaldin@MacBook-Pro-Akmaldin.local>
2022-11-14 11:42:08 +0600

    added register

--- src/Components/Auth/Authorization.jsx
+++ src/Components/Auth/Authorization.jsx
@@ -145,7 +145,6 @@ export default function Authorization() {
             </Grid>
           </Box>
         </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
       </Container>
     </ThemeProvider>
   );
